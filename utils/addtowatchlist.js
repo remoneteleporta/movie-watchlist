3ee3e6288d5c9cb53207a291ef747bb42a0a2b7d
Remone Teleporta <remoneteleporta@gmail.com>
2026-01-15 14:25:14 +0530

    watchlist duplicate fix

--- utils/addtowatchlist.js
+++ utils/addtowatchlist.js
@@ -1,7 +1,20 @@
-let myWatchList = []
+export function addtoWatchlist(addMovie)
+{   
+    if (!addMovie || typeof addMovie !== "object") {
+        console.error("Invalid movie object")
+        return
+    }
 
-export function addtoWatchlist(addMovie){
-    myWatchList.push(addMovie)
- localStorage.setItem("myWatchlist", JSON.stringify(myWatchList) )
- return
+    let localWatchList = JSON.parse(localStorage.getItem("myWatchlist")) || []
+    
+    const movieExists = localWatchList.some(movie => movie.imdbID === addMovie.imdbID)
+    
+    if (movieExists) {
+      console.log("Movie already in Watchlist")
+        return
+    }
+
+    localWatchList.push(addMovie)
+    localStorage.setItem("myWatchlist", JSON.stringify(localWatchList))
+
 }
