8a2dd1700b1febb7d623ef8b56d8ba8ae553701c
Remone Teleporta <remoneteleporta@gmail.com>
2026-01-18 13:09:51 +0530

    add to watchlist fix

--- utils/addtowatchlist.js
+++ utils/addtowatchlist.js
@@ -4,17 +4,16 @@ export function addtoWatchlist(addMovie, newmovieAdded, movienotAdded)
         console.error("Invalid movie object")
         return
     }
-
-    let localWatchList = JSON.parse(localStorage.getItem("myWatchlist")) || [];
     
-    const movieExists = localWatchList.some(movie => movie.imdbID === addMovie.imdbID);
+    let localWatchList = JSON.parse(localStorage.getItem("myWatchlist")) || []
+    
+    const movieExists = localWatchList.some(movie => movie.imdbID === addMovie.imdbID)
     
     if (movieExists) {
         movienotAdded.style.visibility = 'visible'
         setTimeout(()=>{movienotAdded.style.visibility = 'hidden'}, 2000)
         return
     }else{
-
     localWatchList.push(addMovie)
     localStorage.setItem("myWatchlist", JSON.stringify(localWatchList))
     newmovieAdded.style.visibility = 'visible'
--- utils/removefromwatchlist.js
+++ utils/removefromwatchlist.js
@@ -1,10 +1,10 @@
 export function removefromWatchlist(parsedWatchlist, removeMovie, movieRemoved)
 {   
     console.log(parsedWatchlist, removeMovie) 
-    localStorage.clear()
+    localStorage.removeItem("myWatchlist")
     const newWatchList = parsedWatchlist.filter(movie => movie.imdbID !== removeMovie.imdbID)
     console.log(newWatchList)
-    localStorage.setItem("myWatchlist", newWatchList)
+    localStorage.setItem("myWatchlist", JSON.stringify(newWatchList))
     return newWatchList
 
 }
