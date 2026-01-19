f3271705b879cc0adb63c92618f97e888aaab1e0
Remone Teleporta <remoneteleporta@gmail.com>
2026-01-19 15:35:46 +0530

    remove movie fix

--- utils/renderwatchlist.js
+++ utils/renderwatchlist.js
@@ -2,7 +2,7 @@ export function renderWatchlist(watchlistContainer, parsedWatchlist){
 
     console.log(watchlistContainer, parsedWatchlist)
 
-watchlistContainer.innerHTML += parsedWatchlist.map(movie =>{ 
+watchlistContainer.innerHTML = parsedWatchlist.map(movie =>{ 
         return `<div class="movie">
         <img class="movie-poster" alt="${movie.Title}" src="${movie.Poster}" onerror="this.onerror=null; this.src='/assets/default-poster.jpg';">
         <div class="movie-text">
