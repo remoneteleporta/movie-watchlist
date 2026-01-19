739330638a1da22fa416722a4e38498d590dea31
Remone Teleporta <remoneteleporta@gmail.com>
2026-01-19 15:06:53 +0530

    remove movie fix

--- utils/removefromwatchlist.js
+++ utils/removefromwatchlist.js
@@ -3,5 +3,6 @@ export function removefromWatchlist(parsedWatchlist, removeMovie, movieRemoved)
     localStorage.removeItem("myWatchlist")
     const newWatchList = parsedWatchlist.filter(movie => movie.imdbID !== removeMovie.imdbID)
     localStorage.setItem("myWatchlist", JSON.stringify(newWatchList))
+    return
 }
 
